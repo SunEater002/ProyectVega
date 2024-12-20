f78f82ad715e6b2c8a2dfbeb93fe589fce184df8
Beatriz Terwogt <carlos.terwogt19@gmail.com>
2024-12-20 01:19:24 -0600

    20/12/2024 -add flipper -add section questions

--- js/index.js
+++ js/index.js
@@ -3,3 +3,20 @@ document.querySelectorAll('.sec1 .flip-container').forEach(container => {
         container.querySelector('.flipper').classList.toggle('flipped');
     });
 });
+
+
+//questions
+document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+        const faqItem = button.parentElement;
+        const faqAnswer = faqItem.querySelector('.faq-answer');
+
+        faqItem.classList.toggle('active');
+
+        if (faqItem.classList.contains('active')) {
+            faqAnswer.style.maxHeight = faqAnswer.scrollHeight + 'px';
+        } else {
+            faqAnswer.style.maxHeight = '0';
+        }
+    });
+});
