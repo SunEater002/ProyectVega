507bb3578bf879396f4d42c7ef4df74e52afbdb6
Omar <omar.terwogt18@gmail.com>
2025-04-27 21:47:39 -0600

    pagina_actualizada

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,css}'],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#1E40AF',
+      },
+    },
+  },
+  plugins: [],
+};
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.', // Asegúrate de que el directorio raíz sea correcto
+  build: {
+    outDir: 'dist', // Directorio de salida para la compilación
+  },
+});
